5be5d0dd5a016ba16ddf9435651ec4e7e11cd0d4
Antonio  Fodor <108890016+antoniofodor@users.noreply.github.com>
2023-11-28 17:15:15 -0500

    google button styling

--- client/src/views/GoogleAuthLogin/GoogleAuthLogin.jsx
+++ client/src/views/GoogleAuthLogin/GoogleAuthLogin.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { GoogleLogin } from '@react-oauth/google';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { getGoogleLoginUrl, GoogleUserGetProfile} from '../../Utils/GoogleAuthRequests';
+import { setUserSession } from '../../Utils/AuthRequests'
+import { useSearchParams } from 'react-router-dom';
+import { getCurrUser } from '../../Utils/userState';
+import { useNavigate } from 'react-router-dom';
+
+const CLIENT_ID=import.meta.env.VITE_CLIENT_ID;
 
 const GoogleAuthLogin = () => {
+  const [queryParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getUser = async (code) => {
+      return await GoogleUserGetProfile(code);
+    }
+
+    if(queryParams.get('code') !== null) {
+      let userInfo = getUser(queryParams.get('code')).then(data => {
+        let user = data.data.user;
+        let token = data.data.token
+        setUserSession(token, JSON.stringify(user));
+        navigate('/dashboard');        
+      })
+    }
+  }, [])
+  
+  const redirectURL = async () => {
+    const url = await getGoogleLoginUrl().then(res =>{
+      window.location.replace(res.data.url);
+    });
+  }
+
+  const handleLoginCallback = async (e) => {
+    e.preventDefault();
+    redirectURL();
+  }
+
   return (
-    <div style={{ background: 'white', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <button>This will be the google login button</button>
-    </div>
-  );
+      <div className='google-sign-in'>
+        <GoogleOAuthProvider clientId={CLIENT_ID}>
+          <GoogleLogin
+            ux_mode='popup'
+            shape='pill'
+            theme='filled_blue'
+            size='large'
+            onSuccess={credentialRes => { console.log(credentialRes) }}
+            onError={() => console.log("Login Failed")}
+          />
+        </GoogleOAuthProvider>
+      </div>
+  );  
+
 };
 
 export default GoogleAuthLogin;
